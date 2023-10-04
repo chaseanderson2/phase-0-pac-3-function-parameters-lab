9e8e3b1bd31cfa9ca025a9bde9191f2e9c135c43
Chase Anderson <chaseanderson.stu@gmail.com>
2023-10-03 20:28:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -10,15 +10,9 @@ function introductionWithLanguage(name, language) {
 introductionWithLanguage("Aki", "Ember.js");
 introductionWithLanguage("Samip", "React");
 
-function introductionWithLanguageOptional(name, language = "JavaScript") {
+function introductionWithLanguageOptional(name, language = `JavaScript`) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-introductionWithLanguageOptional("Gracie");
-
-
-function introductionWithLanguageOptional(name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-}
+introductionWithLanguageOptional("Gracie", "JavaScript");
 introductionWithLanguageOptional("Gracie", "Python");
-
